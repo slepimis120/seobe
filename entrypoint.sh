00a325147f6fae14e00ff2de7409a10df7cf78b5
slepimis120 <slepimis120@gmail.com>
2025-02-20 11:03:21 +0100

    fix: Change variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ echo "Updating GitHub Actions Importer..."
 gh actions-importer update
 
 echo "Running migrate..."
-gh actions-importer migrate jenkins -o output/audit --source-url https://9e83-82-117-210-226.ngrok-free.app/job/seobe --target-url https://github.com/slepimis120/seobe
+gh actions-importer migrate jenkins -o output/audit --source-url https://9e83-82-117-210-226.ngrok-free.app/job/panoptikon --target-url https://github.com/slepimis120/panoptikon
 
 tail -f /dev/null
